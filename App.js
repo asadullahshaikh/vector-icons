6cbd2a17c27a2557958d04bdce10c06b6a4a4f53
asadullah shaikh <asadullahshaikh81@gmail.com>
2020-04-11 17:36:46 +0500

    vector icons setup

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function ExampleView(props) {
+  return (
+    <>
+      <TouchableOpacity
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'green',
+          flex: 1,
+        }}>
+        <Icon name="person" size={50} color="black" />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'yellow',
+          flex: 1,
+        }}>
+        <Icon name="add-box" size={50} color="black" />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'purple',
+          flex: 1,
+        }}>
+        <Icon name="details" size={50} color="black" />
+      </TouchableOpacity>
+    </>
+  );
+}
+
+export default ExampleView;
